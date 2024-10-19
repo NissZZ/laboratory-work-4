57c2f8f261db86e712de88f007d353e39a5bc5ff
Matvey Ivanov <98405814+NissZZ@users.noreply.github.com>
2024-10-19 20:44:46 +0300

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -4,14 +4,14 @@ using namespace std;
 string replace(const string& str, const string& old, const string& new_string)
 {
     string str_new = str;
-    for (int i = 0; i < str.length(); i++)
+    for (int i = 0; i < str.size(); i++)
     {
         if (str.find(old, i) == string::npos)
         {
             return str_new;
         }
         int index = str.find(old, i);
-        str_new.erase(index, old.length());
+        str_new.erase(index, old.size());
         str_new.insert(index, new_string);
     }
 }
